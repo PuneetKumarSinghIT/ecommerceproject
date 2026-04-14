c494916869885ee9eb9f0e631af0ec8396ab0d4f
PuneetKumarSinghIT <puneetkumarsingh765@gmail.com>
2026-04-14 22:07:43 +0530

    enhanced the fakestore api to provide result

--- src/main/java/com/ecommerce/ecommerseproject/service/FakeStoreProductService.java
+++ src/main/java/com/ecommerce/ecommerseproject/service/FakeStoreProductService.java
@@ -1,7 +1,9 @@
 package com.ecommerce.ecommerseproject.service;
 
+import com.ecommerce.ecommerseproject.dto.FakeStoreProductDto;
 import com.ecommerce.ecommerseproject.models.Product;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -9,9 +11,21 @@ import java.util.List;
 public class FakeStoreProductService implements ProductService{
 
 //    Inside this Fakestore is third party implementation.
+    private RestTemplate restTemplate;
+
+    public FakeStoreProductService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
     public Product getSingleProduct(long productId) {
-        return null;
+
+        System.out.println("We are running the getSingleProduct service in fakestore");
+        FakeStoreProductDto fakestoreproductdto =
+                restTemplate.getForObject("https://fakestoreapi.com/products/" +
+                        productId, FakeStoreProductDto.class );
+        assert fakestoreproductdto != null;
+        return fakestoreproductdto.getProduct();
     }
 
     @Override
