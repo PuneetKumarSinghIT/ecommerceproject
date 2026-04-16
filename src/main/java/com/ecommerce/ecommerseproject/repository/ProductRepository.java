454a4d5a3a9791a25ed6c0930b14ca2382c50f28
PuneetKumarSinghIT <puneetkumarsingh765@gmail.com>
2026-04-17 01:55:05 +0530

    JPA interface implementation

---
+++ src/main/java/com/ecommerce/ecommerseproject/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.ecommerce.ecommerseproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+
+import com.ecommerce.ecommerseproject.models.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // create a query like this "select * form product where description = ?"
+    Product findByDescription(String description);
+    
+    // Create a query like this "select * from product where title = ?"
+    Product findByTitle(String title);
+
+    // This will insert the product record in table.
+    Product save(@NonNull Product product);
+}
